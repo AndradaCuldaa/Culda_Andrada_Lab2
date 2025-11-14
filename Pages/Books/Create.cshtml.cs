a7f6b9a35e7aee67b4b0df1c8fe0df3f9068149f
AndradaCuldaa <andrada.liana.culda@stud.ubbcluj.ro>
2025-11-14 14:33:58 +0200

    Laboratorul 6

--- Pages/Books/Create.cshtml.cs
+++ Pages/Books/Create.cshtml.cs
@@ -2,11 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Culda_Andrada_Lab2.Data;
-using Culda_Andrada_Lab2.Models;
 
 namespace Culda_Andrada_Lab2.Pages.Books
 {
--- Program.cs
+++ Program.cs
@@ -5,7 +5,10 @@ using Microsoft.AspNetCore.Identity;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages(options =>
+{
+    options.Conventions.AuthorizeFolder("/Books");
+});
 builder.Services.AddDbContext<Culda_Andrada_Lab2Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Culda_Andrada_Lab2Context") ?? throw new InvalidOperationException("Connection string 'Culda_Andrada_Lab2Context' not found.")));
 
